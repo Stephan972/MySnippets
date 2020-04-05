87801dd260d38c9bc190136adec2f752ae44cf9d
cession <cession@cession-PC>
2020-04-05 05:52:02 +0200

    - Migrate to Jsoup 1.13.1

--- src/main/java/my/web/WebClient.java
+++ src/main/java/my/web/WebClient.java
@@ -9,10 +9,18 @@ import java.io.IOException;
 import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.hjson.JsonValue;
 import org.jsoup.Connection.Method;
@@ -233,7 +241,7 @@ public class WebClient {
 
                 r = Jsoup.connect(url) //
                         .proxy(proxy.toJavaProxy()) //
-                        .validateTLSCertificates(configuration.validateTLSCertificates()) //
+                        .sslSocketFactory(configuration.validateTLSCertificates() ? null : acceptAnyCertificatesSslSocketFactory()) //
                         .method(method) //
                         .data(data) //
                         .followRedirects(configuration.followRedirects()) //
@@ -268,6 +276,35 @@ public class WebClient {
 
             proxySelector.unbookProxy(proxy, keepUsingProxy);
         }
+    }
+
+    /**
+     * 
+     * @return
+     * 
+     * @see https://stackoverflow.com/a/55531011/363573
+     * 
+     */
+    private SSLSocketFactory acceptAnyCertificatesSslSocketFactory() {
+        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            public  X509Certificate[] getAcceptedIssuers() {
+              return null;
+            }
+
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+          }};
+
+          try {
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            return sslContext.getSocketFactory();
+          } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException("Failed to create a SSL socket factory", e);
+          }
     }
 
     private void haveBreakNowIfNecessary() throws InterruptedException {
